f1cf6f716fbce61b16dacd06475fdfcc287ece44
yan.zhang <13721133018@163.com>
2021-12-01 17:37:54 +0800

    CompletableFuture Demo

--- src/main/java/com/demo/completable/CompletableFutureComposeTest.java
+++ src/main/java/com/demo/completable/CompletableFutureComposeTest.java
@@ -3,8 +3,9 @@ package com.demo.completable;
 import com.demo.mock.ThreadPoolExecutorMock;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 /**
  * @Author yan.zhang
@@ -16,26 +17,50 @@ public class CompletableFutureComposeTest {
         ThreadPoolExecutor executor = ThreadPoolExecutorMock.mock();
 
         CompletableFuture<Integer> future1 = CompletableFuture.supplyAsync(() -> {
-            System.out.println("compute 1...start");
-            try {
-                TimeUnit.SECONDS.sleep(3);
-            } catch (InterruptedException e) {
-
-            }
-            System.out.println("compute 1...end");
+            System.out.println("compute 1");
             return 1;
         });
-        CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(() -> {
-            System.out.println("compute 2..start");
-            try {
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
 
-            }
-            System.out.println("compute 2..end");
-            return 2;
+        //如果任务之间有前后依赖关系，但是连接是独立的CompletableFuture
+        CompletableFuture<CompletableFuture<Integer>> future2 = future1.thenApply((result) -> CompletableFuture.supplyAsync(() -> result + 10));
+
+        //此时获取计算结果
+        //当连接任务越来越多，代码会越来越复杂，引入thenCompose，类比flatMap
+        System.out.println(future2.join().join());
+
+
+        CompletableFuture<Integer> future3 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("compute 3");
+            return 1;
         });
 
-        future1.thenCompose((result) -> CompletableFuture.supplyAsync(() -> result + 10)).join();
+        CompletableFuture<Integer> future4 = future3.thenCompose((r) -> CompletableFuture.supplyAsync(() -> r + 10));
+        System.out.println(future4.join());
+
+
+        /**
+         * <U> CompletionStage<U> thenApply(Function<? super T,? extends U> fn)
+         * <U> CompletionStage<U> thenCompose(Function<? super T,? extends CompletionStage<U>> fn)
+         *
+         * 对于thenApply，fn函数是一个对一个已完成的stage或者说CompletableFuture的的返回值进行计算、操作
+         * 对于thenCompose，fn函数是对另一个CompletableFuture进行计算、操作。
+         */
+
+        CompletableFuture<String> f1 = CompletableFuture.supplyAsync(() -> 100).thenApply(new Function<Integer, String>() {
+            @Override
+            public String apply(Integer num) {
+                return num + " to String";
+            }
+        });
+
+        CompletableFuture<String> f2 = CompletableFuture.supplyAsync(() -> 100).thenCompose(new Function<Integer, CompletionStage<String>>() {
+            @Override
+            public CompletionStage<String> apply(Integer integer) {
+                return CompletableFuture.supplyAsync(() -> integer + " to String");
+            }
+        });
+
+        System.out.println(f1.join());
+        System.out.println(f2.join());
     }
 }
